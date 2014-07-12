71bd2b0b6d50dcc4d514bf5599f9f16d70e837cc
Sharla <sharla@Lou-Manglasss-MacBook-Pro.local>
2014-07-12 08:56:39 -0400

    button now uses boolean for button state

--- button.ino
+++ button.ino
@@ -2,7 +2,7 @@ const int buttonPin = 2;     // the number of the pushbutton pin
 const int ledPin =  13;      // the number of the LED pin
 
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+boolean buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
   // initialize the LED pin as an output:
